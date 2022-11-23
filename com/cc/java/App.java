af1344e2168fe6ab990282d89318eae76de0f08d
Daniela <Daniela-Haeussler@gmx.de>
2022-11-23 17:09:22 +0100

    fertig

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -6,8 +6,15 @@ public class App {
 
     Konto konto1 = new Konto(10000);
     konto1.setKontostand(10000*2);
-    output(String.valueOf(konto1.getKontostand()));
+    output("Aktueller Kontostand :" +String.valueOf(konto1.getKontostand()+" EUR"));
+
+    Konto konto2 = new Konto(10000);
+    konto2.setKontostand(10000*3);
+    output("Aktueller Kontostand :" + String.valueOf(konto2.getKontostand()+" EUR"));
     
+    Konto konto3 = new Konto(10000);
+    konto3.setKontostand(10000*10);
+    output("Aktueller Kontostand :" + String.valueOf(konto3.getKontostand()+" EUR") );
     }
 
     public static void output(String outputStr) {
